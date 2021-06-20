9e7a1003174d07996d7904d962bf0ea7f2347722
Helen mary varghese <65602897+helen579@users.noreply.github.com>
2021-06-21 00:38:01 +0530

    Update PositiveNo.py

--- PositiveNo.py
+++ PositiveNo.py
@@ -9,13 +9,10 @@ while(num1 < len(list1)):
     if list1[num1] >= 0:
         print(list1[num1], end = " ")
     num1 += 1
-
 print("\n")
-
 print("list2 = ", end = " ")
 print(list2)
 while(num2 < len(list2)):
     if list2[num2] >= 0:
         print(list2[num2], end = " ")
-
     num2 += 1
